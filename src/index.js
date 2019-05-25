917cea43ef6a6811ed7b57d7f16480f4fbb60231
Dino Kong <dino.kong@gmail.com>
2019-05-25 22:32:30 +0800

    added integration test

--- src/index.js
+++ src/index.js
@@ -1,62 +1,6 @@
-
-/**
- * Module dependencies.
- */
-
-import debugLib from 'debug';
-import http from 'http';
-
-import app from './app';
+import server from './server';
 import connectMongoDB from './mongodb';
 import { port } from '../config';
-
-const debug = debugLib('url-shorten-service:server');
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-const onError = (error) => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  const bind = typeof port === 'string'
-    ? `Pipe ${port}`
-    : `Port ${port}`;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(`${bind} requires elevated privileges`);
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(`${bind} is already in use`);
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? `pipe ${addr}`
-    : `port ${addr.port}`;
-  debug(`Listening on ${bind}`);
-};
-
-/**
- * Get port from environment and store in Express.
- */
-
-app.set('port', port);
 
 /**
  * Create MongoDB connection
@@ -64,18 +8,4 @@ app.set('port', port);
 
 connectMongoDB();
 
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-export default server;
---
+++ src/integrationTests/url.test.js
@@ -0,0 +1,76 @@
+/* eslint-disable arrow-body-style */
+import request from 'supertest';
+import httpStatus from 'http-status';
+import { expect } from 'chai';
+import Chance from 'chance';
+import shortid from 'shortid';
+
+import server from '../server';
+import connectMongoDB from '../mongodb';
+import Url from '../models/url-model';
+import { port } from '../../config';
+
+const chance = Chance();
+
+describe('Url Shortening', async () => {
+  let connection;
+
+  before(() => {
+    connection = connectMongoDB();
+    server.listen(port);
+  });
+
+  after(() => {
+    connection.close();
+    server.close();
+  });
+
+  describe('GET /:code', () => {
+    let urlList;
+
+    beforeEach(async () => {
+      urlList = new Array(chance.integer({ min: 20, max: 100 }))
+        .fill(null).map(() => ({
+          url: chance.url(),
+          code: shortid.generate(),
+        }));
+      await Url.deleteMany({});
+      await Url.insertMany(urlList);
+    });
+
+    it('should get url redirection correctly', async () => {
+      const target = await Url.findOne({});
+      return request(server)
+        .get(`/${target.code}`)
+        .expect(httpStatus.FOUND)
+        .then((res) => {
+          expect(res.headers.location).to.be.equal(target.url);
+        });
+    });
+
+    it('should report error when the shortened url does not exist', () => {
+      return request(server)
+        .get('/youarenotgonnafindme')
+        .expect(httpStatus.NOT_FOUND);
+    });
+  });
+
+  describe('POST /', () => {
+    const testUrl = chance.url();
+    const testCode = shortid.generate();
+    it('saves an url correctly with a shorten url return', () => {
+      return request(server)
+        .post('/')
+        .send({
+          url: testUrl,
+          code: testCode,
+        })
+        .expect(httpStatus.OK)
+        .then((res) => {
+          const { url, code } = res.body;
+          expect(url).to.be.equal(testUrl);
+          expect(code).to.be.equal(code);
+        });
+    });
+  });
+});
