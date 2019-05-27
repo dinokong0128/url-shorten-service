2dc9026b7706ac8c0fb2b05e2ae1d1df9f263e40
Dino Kong <dino.kong@gmail.com>
2019-05-27 11:06:04 +0800

    fixed url submission form

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,6 +1,6 @@
 
 const isValidUrl = (url) => {
-  const pattern = new RegExp('^(https?:\\/\\/)?' // protocol
+  const pattern = new RegExp('^(https?:\\/\\/)' // protocol
     + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}| ' // domain name
     + '((\\d{1,3}\\.){3}\\d{1,3}))' // ip (v4) address
     + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // port
